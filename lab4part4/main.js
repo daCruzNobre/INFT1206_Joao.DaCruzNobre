d290322d993be07321b90139a6273ae6b965d345
daCruzNobre <98597465+daCruzNobre@users.noreply.github.com>
2023-11-24 21:43:50 -0500

    Update main.js
    added draw method in EvilCircle class

--- lab4part4/main.js
+++ lab4part4/main.js
@@ -110,6 +110,13 @@ class EvilCircle extends Shape {
   }
 
   // EvilCircle Methods
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
 
 const balls = [];
