1ddcf8b56a0ccf5865109955d372b0c1c05ee6ba
daCruzNobre <98597465+daCruzNobre@users.noreply.github.com>
2023-11-24 21:55:20 -0500

    Update main.js
    added uptaded collisionDetect method to EvilCircle

--- lab4part4/main.js
+++ lab4part4/main.js
@@ -136,6 +136,20 @@ class EvilCircle extends Shape {
     }
   
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
+  }
 }
 
 const balls = [];
